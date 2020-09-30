ff0aa7e4c9c979d8e3985b9ca1917414cc998d2a
Aakarsh Dhawan <aakarsh.dhawan@grapecity.com>
2020-10-01 02:34:23 +0530

    NameCalculator angularjs web app
    First application - Name Calculator

---
+++ NameCalculator/app.js
@@ -0,0 +1,25 @@
+(function(){
+'use strict';
+
+angular.module('NameCalculator',[])
+
+.controller('NameCalculatorController', function($scope){
+$scope.name="Sammy";
+$scope.totalValue=0;
+$scope.DisplayNumeric=function(){
+  var totalNameValue=calculateNumericForString($scope.name);
+  $scope.totalValue=totalNameValue;
+};
+$scope.init=function(){
+  $scope.DisplayNumeric();
+};
+function calculateNumericForString(string){
+  var totalStringValue=0;
+  for(var i=0;i<string.length;i++){
+    totalStringValue+=string.charCodeAt(i);
+  }
+  return totalStringValue;
+}
+});
+
+})();
