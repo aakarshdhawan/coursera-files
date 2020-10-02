ffa3eecf3ffa440aa0854c420e4e230eb3cc14c8
Aakarsh Dhawan <aakarsh.dhawan@grapecity.com>
2020-10-02 15:04:29 +0530

    formatting updated

--- NameCalculator/app.js
+++ NameCalculator/app.js
@@ -3,23 +3,25 @@
 
 angular.module('NameCalculator',[])
 
-.controller('NameCalculatorController', function($scope){
-$scope.name="Sammy";
-$scope.totalValue=0;
-$scope.DisplayNumeric=function(){
-  var totalNameValue=calculateNumericForString($scope.name);
-  $scope.totalValue=totalNameValue;
-};
-$scope.init=function(){
-  $scope.DisplayNumeric();
-};
-function calculateNumericForString(string){
-  var totalStringValue=0;
-  for(var i=0;i<string.length;i++){
-    totalStringValue+=string.charCodeAt(i);
-  }
-  return totalStringValue;
-}
-});
+.controller('NameCalculatorController', NameCalculatorController);
 
+NameCalculatorController.$inject=['$scope'];
+function NameCalculatorController($scope){
+  $scope.name="Sammy";
+  $scope.totalValue=0;
+  $scope.DisplayNumeric=function(){
+    var totalNameValue=calculateNumericForString($scope.name);
+    $scope.totalValue=totalNameValue;
+  };
+  $scope.init=function(){
+    $scope.DisplayNumeric();
+  };
+  function calculateNumericForString(string){
+    var totalStringValue=0;
+    for(var i=0;i<string.length;i++){
+      totalStringValue+=string.charCodeAt(i);
+    }
+    return totalStringValue;
+  }
+}
 })();
